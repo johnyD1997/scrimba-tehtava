e72d83dc7ec5e21f86bd628feddb842e60c145e2
Ваня Дубинников <ivan.dubinnikov@gmail.com>
2023-03-15 11:34:11 +0200

    Adddone project

--- scrimba-tehtava/src/app/app.module.ts
+++ scrimba-tehtava/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CustomersModule } from './customers/customer.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+import { OrdersModule } from './orders/orders.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CustomersModule,
+    OrdersModule,
+    CoreModule,
+    SharedModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
